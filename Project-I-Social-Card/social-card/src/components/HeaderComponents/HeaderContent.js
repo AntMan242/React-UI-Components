0c262bdf96c319c3b77469a8f8422903a2304a1e
anthony <anthony24.kwhomes@gmail.com>
2019-03-25 15:40:02 -0700

    Initial project commit. Working on components.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+
+const Header = () => {
+    return (
+        <div className = "Header">
+            <h1>Lambda School @LambdaSchool 25 mar</h1>
+        </div>
+    )
+}
+
+export default Header;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+
+const Title = () => {
+    return (
+        <title>Social Card</title>
+    )
+}
+
+export default Title;
